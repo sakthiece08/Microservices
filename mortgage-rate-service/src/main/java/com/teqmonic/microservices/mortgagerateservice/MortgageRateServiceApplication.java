e25ff24108cf5b9b13571239d5217a090381777a
sakthiece08 <41096775+sakthiece08@users.noreply.github.com>
2023-09-20 12:33:08 -0400

    Initial version of service

---
+++ mortgage-rate-service/src/main/java/com/teqmonic/microservices/mortgagerateservice/MortgageRateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teqmonic.microservices.mortgagerateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageRateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageRateServiceApplication.class, args);
+	}
+
+}
---
+++ mortgage-rate-service/src/test/java/com/teqmonic/microservices/mortgagerateservice/MortgageRateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teqmonic.microservices.mortgagerateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageRateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
