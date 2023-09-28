19cef9a979519cdec86b406be00403751e69bd6a
sakthiece08 <41096775+sakthiece08@users.noreply.github.com>
2023-09-28 11:15:59 -0400

    Api gateway service

---
+++ api-gateway/src/main/java/com/teqmonic/microservices/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.teqmonic.microservices.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
