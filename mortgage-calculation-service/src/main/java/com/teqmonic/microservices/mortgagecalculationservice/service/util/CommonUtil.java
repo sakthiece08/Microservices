7bbd77debda82fb7d5466c4d9a56b756f4598394
sakthi <sakthiece08@gmail.com>
2023-11-15 16:31:08 -0500

    Read json file as Inputstream instead of File for Dockerization

--- mortgage-calculation-service/src/main/java/com/teqmonic/microservices/mortgagecalculationservice/service/util/CommonUtil.java
+++ mortgage-calculation-service/src/main/java/com/teqmonic/microservices/mortgagecalculationservice/service/util/CommonUtil.java
@@ -2,10 +2,11 @@ package com.teqmonic.microservices.mortgagecalculationservice.service.util;
 
 import static com.teqmonic.microservices.mortgagecalculationservice.service.util.Constants.MOCK_RESPONSE_PATH;
 
-import java.io.File;
+import java.io.InputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,15 +17,17 @@ public class CommonUtil {
 	public static <T> T convertJsonToJava(String fileName, Class<T> classType) {
 		logger.info("In convertJsonToJava, fileName {}, classType {} ", fileName, classType);
 		T type = null;
-		File file = new File(MOCK_RESPONSE_PATH + fileName);
+		InputStream input;
 		try {
+			input = new ClassPathResource(MOCK_RESPONSE_PATH + fileName).getInputStream();
 			ObjectMapper mapper = new ObjectMapper();
 			mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-			type = mapper.readValue(file, classType);
+			type = mapper.readValue(input, classType);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return type;
 	}
+
 }
